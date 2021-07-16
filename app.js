622f324850fc926055e741e0408a7c5cce8abd66
Your Name <you@example.com>
2021-07-16 16:51:50 +0530

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+$('.slick-carousel').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 6, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    arrows:true,
+    nextArrow: '<img src="images/left.svg" class="leftarr" alt="">',
+    prevArrow: '<img src="images/right.svg" class="rightarr" alt="">'
+
+  });
